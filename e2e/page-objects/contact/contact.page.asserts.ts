e7c9e18cb5b6932059bafa68aedc651500e25fe1
wambookontakt@gmail.com <wambookontakt@gmail.com>
2024-01-29 18:13:54 +0000

    contact page fix

--- e2e/page-objects/contact/contact.page.asserts.ts
+++ e2e/page-objects/contact/contact.page.asserts.ts
@@ -45,7 +45,6 @@ export class ContactPageAsserts {
 
         for (const input of inputs) {
             await expect(input).toHaveClass(/et_contact_error/);
-            // test e2e
         }
     }
 }
